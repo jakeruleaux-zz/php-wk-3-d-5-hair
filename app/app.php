72ba5c357a688794f55a90e4db6c5bbe7e959b8d
jacob ruleaux <jakeruleaux@gmail.com>
2017-07-14 08:26:59 -0700

    add database tables

---
+++ app/app.php
@@ -0,0 +1,15 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Stylist.php";
+
+    $app = new Silex\Application();
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+
+
+
+    return $app;
+?>
