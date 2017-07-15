0d0aaf8852bc71cc7d0b2152c6e2aed0e2f4c2b6
jacob ruleaux <jakeruleaux@gmail.com>
2017-07-15 12:40:23 -0700

    debug app file

--- app/app.php
+++ app/app.php
@@ -1,11 +1,13 @@
 <?php
+
+    date_default_timezone_set('America/Los_Angeles');
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Stylist.php";
     require_once __DIR__."/../src/Client.php";
 
     $app = new Silex\Application();
 
-   $server = 'mysql:host=localhost:8889;dbname=to_do';
+   $server = 'mysql:host=localhost:8889;dbname=hair_salon';
    $username = 'root';
    $password = 'root';
    $DB = new PDO($server, $username, $password);
