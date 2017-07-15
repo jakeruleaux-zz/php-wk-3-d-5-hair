92ae2c3528731822e6260683e5f7800aa81e6008
jacob ruleaux <jakeruleaux@gmail.com>
2017-07-14 17:16:37 -0700

    build out app file

--- app/app.php
+++ app/app.php
@@ -16,6 +16,48 @@
         'twig.path' => __DIR__.'/../views'
     ));
 
+    $app->get("/", function() use ($app) {
+      return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll()));
+  });
+
+  $app->get("/clients", function() use ($app) {
+      return $app['twig']->render('clients.html.twig', array('clients' => Client::getAll()));
+  });
+
+  $app->post("/client", function() use ($app) {
+      $client_name = $_POST['client_name'];
+      $Stylist_id = $_POST['stylist_id'];
+      $client = new Client($client_name, $id = null, $stylist_id);
+      $client->save();
+      $stylist = stylist::find($stylist_id);
+      return $app['twig']->render('stylist.html.twig', array('stylists' => $stylist, 'clients' => $stylist->getClients()));
+  });
+
+  $app->get("/stylists", function() use ($app) {
+      return $app['twig']->render('stylists.html.twig', array('stylists' => Sylist::getAll()));
+  });
+
+  $app->get("/stylists/{id}", function($id) use ($app) {
+      $stylist = Stylist::find($id);
+      return $app['twig']->render('stylist.html.twig', array('stylists' => $stylist, 'clients' => $stylist->getClients()));
+  });
+
+  $app->post("/stylists", function() use ($app) {
+      $stylist = new Stylist($_POST['name']);
+      $stylist->save();
+      return $app['twig']->render('stylists.html.twig', array('stylists' => Stylist::getAll()));
+  });
+
+  $app->post("/delete_stylists", function() use ($app) {
+      Stylist::deleteAll();
+      return $app['twig']->render('index.html.twig');
+  });
+
+  $app->post("/delete_clients", function() use ($app) {
+      Client::deleteAll();
+      return $app['twig']->render('index.html.twig');
+  });
+
 
 
 
