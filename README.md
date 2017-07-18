01aecfb0a1300fd40df77788b18415f80840e1eb
Guest User <Guest@Epicodus-7A.local>
2017-07-18 07:35:04 -0700

    update src and app files

--- README.md
+++ README.md
@@ -4,42 +4,42 @@ Hair Salon App, 07/14/17
 
 By Jacob Ruleaux
 
-Description
+* Description;
 
-The Hair Salon app shows current stylists as well as a list of each stylists' clients. The progam will allow user to add stylists or clients, or delete either.
+* The Hair Salon app shows current stylists as well as a list of each stylists' clients. The progam will allow user to add stylists or clients, or delete either.
 
-Setup/Installation Requirements
+* Setup/Installation Requirements
 
-Open GitHub site on your browser: https://github.com/jakeruleaux/php-wk-3-d-5-hair
-Select the dropdown (green box) "Clone or download"
-Copy the link for the GitHub repository
-Open Terminal on your computer
-In Terminal, perform the following steps:
-Type 'cd desktop' and press enter.
-Type 'git clone' then copy the repository link and press enter.
-Type 'cd php-address-book' to access the path on your computer.
-Type 'localhost:8888/phpmyadmin' and select the import tab near the top of the screen. In the import tab browse for 'hair_salon.sql.zip'.
-Select this file and click the 'go' button at the bottom.
-In Terminal type /Applications/MAMP/Library/bin/mysql --host=localhost -uroot -proot.
-In mysql type 'SHOW DATABASES;' to confirm that you have the 'hair_salon' database.
-In mysql type 'USE hair_salon;'.
-In your Address Bar type 'localhost:8888' to view app.
-The paths in MAMP may need to be adjusted. In MAMP click the 'web server' tab. Make sure you have the appropriate document root path, ex: 'User/File-directory/file/web'.
+* Open GitHub site on your browser: https://github.com/jakeruleaux/php-wk-3-d-5-hair
+* Select the dropdown (green box) "Clone or download"
+* Copy the link for the GitHub repository
+* Open Terminal on your computer
+* In Terminal, perform the following steps:
+* Type 'cd desktop' and press enter.
+* Type 'git clone' then copy the repository link and press enter.
+* Type 'cd php-wk-3-d-5-hair' to access the path on your terminal.
+* Type 'localhost:8888/phpmyadmin' and select the import tab near the top of the screen. In the import tab browse for 'hair_salon.sql.zip'.
+* Select this file and click the 'go' button at the bottom.
+* In Terminal type /Applications/MAMP/Library/bin/mysql --host=localhost -uroot -proot.
+* In mysql type 'SHOW DATABASES;' to confirm that you have the 'hair_salon' database.
+* In mysql type 'USE hair_salon;'.
+* In your Address Bar type 'localhost:8888' to view app.
+* The paths in MAMP may need to be adjusted. In MAMP click the 'web server' tab. Make sure you have the appropriate document root path, ex: 'User/File-directory/file/web'.
 
-Known Bugs
+* Known Bugs
 
-The program requires a localhost to function. It was designed with MAMP in mind. Similar programs may support it.
+* The program requires a localhost to function. It was designed with MAMP in mind. Similar programs may support it.
 
-Support and contact details
+* Support and contact details
 
 Feel free to contact the author with questions or concerns at jakeruleaux@hotmail.com
 
-Technologies Used
+* Technologies Used
 
 The application relies on MAMP, PHP, Silex, Twig with some Bootstrap for styling and basic HTML for display.
 
-License
+* License
 
 MIT
 
-Copyright (c) 2017 Jacob Ruleaux
+* Copyright (c) 2017 Jacob Ruleaux
--- app/app.php
+++ app/app.php
@@ -4,9 +4,10 @@
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Stylist.php";
     require_once __DIR__."/../src/Client.php";
-
+    use Symfony\Component\Debug\Debug;
+    Debug::enable();
     $app = new Silex\Application();
-
+    $app['debug'] = true;
    $server = 'mysql:host=localhost:8889;dbname=hair_salon';
    $username = 'root';
    $password = 'root';
@@ -29,24 +30,24 @@
           return $app['twig']->render('clients.html.twig', array('clients' => Client::getAll()));
       });
 
-      $app->post("/clients", function() use ($app) {
+      $app->post('/clients', function() use ($app) {
           $client_name = $_POST['client_name'];
           $stylist_id = $_POST['stylist_id'];
-          $client = new Client($client_name, $stylist_id, $id = null);
+          $client = new Client($client_name, $stylist_id);
           $client->save();
         //   var_dump($client);
           $stylist = Stylist::find($stylist_id);
           return $app['twig']->render('stylists.html.twig', array('stylist' => $stylist, 'clients' => $stylist->getClients()));
       });
 
-      $app->get("/stylists", function() use ($app) {
-          return $app['twig']->render('stylists.html.twig', array('stylists' => Stylist::getAll()));
-      });
+    //   $app->get("/stylists", function() use ($app) {
+    //       return $app['twig']->render('stylists.html.twig', array('stylists' => Stylist::getAll()));
+    //   });
 
-      $app->get("/stylists/{id}", function($id) use ($app) {
-          $stylist = Stylist::find($id);
-          return $app['twig']->render('stylist.html.twig', array('stylists' => $stylist, 'clients' => $stylist->getClients()));
-      });
+    //   $app->get("/stylists/{id}", function($id) use ($app) {
+    //       $stylist = Stylist::find($id);
+    //       return $app['twig']->render('stylist.html.twig', array('stylist' => $stylist, 'clients' => $stylist->getClients()));
+    //   });
 
       $app->post("/stylists", function() use ($app) {
           $stylist = new Stylist($_POST['stylist_name']);
@@ -54,33 +55,33 @@
           return $app['twig']->render('stylists.html.twig', array('stylists' => Stylist::getAll()));
       });
 
-      $app->get("/stylists/{id}/edit", function($id) use ($app) {
-          $stylist = Stylist::find($id);
-          return $app['twig']->render('stylist_edit.html.twig',   array('stylists' => $stylist));
-      });
+    //   $app->get("/stylists/{id}/edit", function($id) use ($app) {
+    //       $stylist = Stylist::find($id);
+    //       return $app['twig']->render('stylist_edit.html.twig',   array('stylists' => $stylist));
+    //   });
 
-      $app->patch("/stylists/{id}", function($id) use ($app) {
-          $stylist_name = $_POST['stylist_name'];
-          $stylist = Stylist::find($id);
-          $stylist->update($stylist_name);
-          return $app['twig']->render('stylist.html.twig', array('stylists' => $stylist, 'clients' => $stylist->getClients()));
-      });
+    //   $app->patch("/stylists/{id}", function($id) use ($app) {
+    //       $stylist_name = $_POST['stylist_name'];
+    //       $stylist = Stylist::find($id);
+    //       $stylist->update($stylist_name);
+    //       return $app['twig']->render('stylist.html.twig', array('stylists' => $stylist, 'clients' => $stylist->getClients()));
+    //   });
 
-      $app->delete("/stylists/{id}", function($id) use ($app) {
-          $stylist = Stylist::find($id);
-          $stylist->delete();
-          return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll()));
-    });
+    //   $app->delete("/stylists/{id}", function($id) use ($app) {
+    //       $stylist = Stylist::find($id);
+    //       $stylist->delete();
+    //       return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll()));
+    // });
 
-      $app->post("/delete_stylists", function() use ($app) {
-          Stylist::deleteAll();
-          return $app['twig']->render('index.html.twig');
-      });
+    //   $app->post("/delete_stylists", function() use ($app) {
+    //       Stylist::deleteAll();
+    //       return $app['twig']->render('index.html.twig');
+    //   });
 
-      $app->post("/delete_clients", function() use ($app) {
-          Client::deleteAll();
-          return $app['twig']->render('index.html.twig');
-      });
+    //   $app->post("/delete_clients", function() use ($app) {
+    //       Client::deleteAll();
+    //       return $app['twig']->render('index.html.twig');
+    //   });
 
 
 
